1df0de5f095204a308c08bdb5c72c7c75bab26bd
tjacek <tjacek@student.agh.edu.pl>
2017-09-03 23:15:02 +0200

    read paths

--- exper.py
+++ exper.py
@@ -5,7 +5,12 @@ from sklearn import preprocessing
 
 def exper_single(paths,norm=False,select=False,cls_type='svm'):
     data=lasso_selection(paths,select,norm)
+    print(data.X.shape)
     even_data,odd_data=split_data(data)
+    print(even_data.X.shape)
+    print(odd_data.X.shape)
+    print(even_data.y)
+    print(odd_data.y )   
     eval.determistic_eval(odd_data,even_data,cls_type=cls_type)
 
 def experiment_restricted(paths,cats=[],cls_type='svm',to_zero=True):
@@ -17,8 +22,8 @@ def experiment_restricted(paths,cats=[],cls_type='svm',to_zero=True):
     odd_data,even_data=split_data(r_data)
     eval.determistic_eval(odd_data,even_data,cls_type=cls_type)
 
-def experiment_basic(paths,select,norm,cls_type='svm'):
-    data=dataset.read_and_unify(paths,select,norm)
+def experiment_basic(paths,cls_type='svm'):
+    data=lasso_selection(paths[0],'pca',False)
     even_data,odd_data=split_data(data)
     eval.determistic_eval(odd_data,even_data,cls_type=cls_type)
 
@@ -38,21 +43,52 @@ def split_data(r_data):
 def single_dataset(paths):
     all_datasets=[ dataset.get_dataset(path_i) 
                    for path_i in paths]
+    for data_i in all_datasets:
+        print(data_i.X.shape)
+        #data_i.y,data_i.person=data_i.person,data_i.y
     data=dataset.unify_feat(all_datasets)
     return data
 
-if __name__ == "__main__":
-    #in_path1= "../exper2/united/dataset.txt"
-    #in_path2= "../exper2/proj/dataset.txt"
+def read_paths(conf_file):
+    return [ line_i.replace('\n','') 
+             for line_i in open(conf_file,"r")]
 
-    in_path1= '../exper_4/dataset.txt'
-    in_path2= '../final_paper/MSRaction/simple/dataset.txt'
-    in_path3='../final_paper/MSRaction/basic_nn/dataset.txt'
-    paths=[in_path1,in_path2,in_path3]#,in_path3]
-    #exper_single(paths,norm=True,select='lasso',cls_type='svm')
-    A1=[2,3,5,6,10,13,18,20]
-    A2=[1,4,7,8,9,11,12,14]
-    A3=[6,14,15,16,17,18,19,20]
-    A=[i for i in range(20)]
-    experiment_restricted(paths,cats=A3,cls_type='svm')
+if __name__ == "__main__":
+    #in_path_s='../konf/full/dataset.txt'
+    #in_path_s2='../konf/simple/dataset.txt'
+
+    #in_path1= '../exper_4/dataset.txt'
+    #in_path2= '../final_paper/MSRaction/simple/dataset.txt'
+    #in_path3='../final_paper/MSRaction/basic_nn/dataset.txt'
+    #paths=[in_path1,in_path2,in_path_s]#
+    
+    #in_path0="../konf1/dtw_feat.txt"
+    #in_path1="../konf1/dataset.txt"
+    #in_path2="../konf1/simple.txt"
+
+    #in_path1='../konf3/dtw_feats.txt'
+    #in_path2='../konf3/simple.txt'
+    #in_path3='../konf3/max_z.txt'
+
+    #in_path1="../methods/Vb/dtw_feats.txt"
+    #in_path2="../methods/Vb/hf_feats.txt"
+    #in_path='../konf4/dtw_feats.txt'
+    
+    in_path3="../AArtyk/untime/nn/dtw_dataset.txt"
+    #in_path3= '../final_paper/MSRaction/simple/dataset.txt'
+    #in_path3= '../exper_4/dataset.txt'
+    in_path1="../AArtyk/simple/max_z/dtw_dataset.txt"
+    in_path2="../AArtyk/simple/corl/dtw_dataset.txt"
+    #in_path3="../AArtyk/simple/skew/dtw_dataset.txt"
+    
+    in_path_s="../AArtyk/simple/all/simple.txt"
+
+    paths=read_paths('conf/deep.txt')#[in_path1,in_path2,in_path_s]#,in_path3]
+    exper_single(paths,norm=True,select=150,cls_type='svm')
+    
+    #A1=[2,3,5,6,10,13,18,20]
+    #A2=[1,4,7,8,9,11,12,14]
+    #A3=[6,14,15,16,17,18,19,20]
+    #A=[i for i in range(20)]
+    #experiment_restricted(paths,cats=A3,cls_type='svm')
     
