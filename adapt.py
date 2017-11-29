90da68302c505465af452ac56d8c1c0ed57aec7b
tjacek <tjacek@student.agh.edu.pl>
2017-11-29 02:45:54 -0900

    remove restricted set

--- adapt.py
+++ adapt.py
@@ -1,10 +1,8 @@
 from sets import Set
 import exper,dataset,eval,voting
 
-def ensemble_dataset(basic_paths,adapt_paths,rest_sets=None):
+def ensemble_dataset(basic_paths,adapt_paths):
     basic_data=basic_dataset(basic_paths)
-    if(rest_sets==None):
-        rest_sets=[None for path_i in adapt_paths]
     ensemble_datasets=adapt_datasets(adapt_paths,rest_sets)
     return [data_i + basic_data
                 for data_i in ensemble_datasets]
@@ -18,40 +16,22 @@ def basic_dataset(paths,n_feats=150):
 
 def adapt_datasets(paths,rest_sets,n_feats=50):
     if(len(paths)!=len(rest_sets)):
-        raise Exception("paths and rest_sets have different lenght")
-    
+        raise Exception("paths and rest_sets have different lenght")    
     if(type(n_feats)==int):
         n_feats=('rfe',n_feats)
     data=[adapt_data( n_feats,path_i,rest_set_i)
             for path_i,rest_set_i in zip(paths,rest_sets)]
     return data        
 
-def adapt_data(n_feats,path_i,restr_set):
+def adapt_data(n_feats,path_i):
     if(type(path_i)!=list):
         path_i=[path_i]
     data=exper.single_dataset(path_i)
     old_y=data.y
-    if(restr_set!=None):
-        data=restrict_cats(data,restr_set)
     if(n_feats!=None):
         data=exper.feat_selection(data,n_feats,norm=True)
     data.y=old_y
-    return data
-
-def restrict_cats(data,restr_set):
-    if(type(restr_set)==int):
-        restr_set=[restr_set]
-    if(type(restr_set)!=Set):
-        restr_set=Set(restr_set)	
-    def helper(cat_i):
-        if(cat_i in restr_set):
-            return cat_i
-        else:
-            return 0	
-    new_cats=[ helper(cat_i)
-                for cat_i in data.y]
-    data.y=new_cats
-    return data               
+    return data           
 
 def gen_pahts(nn_path,indices):
     if(type(indices)==int):
