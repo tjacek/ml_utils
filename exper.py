4c6d21b9c3fc9432b33a74075a8e54114b515dbb
tjacek <tjacek@student.agh.edu.pl>
2019-07-04 21:00:15 +0200

    test predictions

--- exper.py
+++ exper.py
@@ -1,15 +1,17 @@
 import feats
-#import dataset
-#import eval
-#import select_feat
-#from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 #from sets import Set
 
 def exper_single(in_path):
     feat_dataset=feats.read(in_path)
+    feat_dataset.norm()
     train,test=split_data(feat_dataset)
-    print(train.X.shape)
-    print(test.X.shape)
+    clf=LogisticRegression()
+    clf.fit(train.X,train.get_labels())
+    y_pred=clf.predict(test.X)
+    y_true=test.get_labels()
+    print(classification_report(y_true, y_pred,digits=4))
 
 def split_data(feat_dataset):
     train,test={},{}
@@ -17,39 +19,12 @@ def split_data(feat_dataset):
     for name_i,data_i in feat_dict.items():
         if(person_selector(name_i)):
             train[name_i]=data_i
-            print(name_i)
         else:
             test[name_i]=data_i
     return feats.from_dict(train),feats.from_dict(test)
 
 def person_selector(name_i):
     return (int(name_i.split('_')[1])%2)==1 
-#def experiment_restricted(paths,cats=[],cls_type='svm',to_zero=True):
-#    if(to_zero):
-#        cats=[ (cat_i-1) 
-#               for cat_i in cats]
-#    dataset=single_dataset(paths)   
-#    r_data=dataset.select_category(data,cats)
-#    odd_data,even_data=split_data(r_data)
-#    eval.determistic_eval(odd_data,even_data,cls_type=cls_type)
-
-#def experiment_basic(data,cls_type='svm'):
-#    data=lasso_selection(paths[0],'pca',False)
-#    print(data.y)
-#    even_data,odd_data=split_data(data)
-#    eval.determistic_eval(odd_data,even_data,cls_type=cls_type)
-
-#def feat_selection(data,select=True,norm=True):
-#    if(norm):
-#        data=data(preprocessing.scale)        
-#    if(select==False or select is None):
-#        return data
-#    n_feats=select[1]
-#    if(data.dim()<n_feats):
-#        print(select)
-#        print("No selection only %d features required" % data.dim())
-#        return data 
-#    return select_feat.select_feat(data,select)
 
 if __name__ == "__main__":
     exper_single("btf.txt")
--- feats.py
+++ feats.py
@@ -1,16 +1,23 @@
 import numpy as np 
 import scipy.stats
 import re
+from sklearn import preprocessing
 
 class FeatureSet(object):
     def __init__(self,X,info):
         self.X=X
         self.info=info
     
+    def get_labels(self):
+        return [ int(info_i.split('_')[0]) for info_i in self.info]
+
     def to_dict(self):
         return { self.info[i]:x_i 
                     for i,x_i in enumerate(self.X)}
 
+    def norm(self):
+        self.X=preprocessing.scale(self.X)
+    
     def save(self,out_path,decimals=4):
         lines=[ np.array2string(x_i,separator=",",precision=decimals) for x_i in self.X]
         lines=[ line_i.replace('\n',"")+'#'+info_i 
