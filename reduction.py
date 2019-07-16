cc121c8048ccb0eb0227ea08970febdef1813fd9
tjacek <tjacek@student.agh.edu.pl>
2019-07-16 12:30:25 +0200

    plot  embedding

--- reduction.py
+++ reduction.py
@@ -1,70 +1,38 @@
-import dataset
-import plot
+import feats
 from sklearn import manifold
-import sklearn
-import sys
+import matplotlib.pyplot as plt
 import numpy as np
 
-def tsne_reduction(data,dim=2,change=False):
-    tsne = manifold.TSNE(n_components=dim, init='pca', random_state=0)
-    X_prim=tsne.fit_transform(data.X)
-    if(change):
-        data.X=X_prim
-        return data
-    return X_prim
+def show_feats(in_path):
+    feat_dataset=feats.read(in_path)
+    tsne=manifold.TSNE(n_components=2,perplexity=30)#init='pca', random_state=0)
+    X=tsne.fit_transform(feat_dataset.X)
+    y=feat_dataset.get_labels()
+    color_helper=lambda i,y_i:y_i 
+    plot_embedding(X,y,title="tsne",color_helper=color_helper,show=True)
 
-def lle_reduction(data,dim=2,n_neighbors=20):
-    lle= manifold.LocallyLinearEmbedding(n_neighbors, n_components=dim,
-                                      method='standard')
-    X_prim=lle.fit_transform(data.X)
-    return X_prim
+def plot_embedding(X,y,title="plot",color_helper=None,show=True):
+    n_points=X.shape[0]
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+   
+    color_helper=color_helper if(color_helper) else lambda i,y_i:0
 
-def hessian_reduction(data,dim=2,n_neighbors=15):
-    lle= manifold.LocallyLinearEmbedding(n_neighbors, n_components=dim,
-                                      method='hessian',eigen_solver="dense")
-    X_prim=lle.fit_transform(data.X)
-    return X_prim
+    plt.figure()
+    ax = plt.subplot(111)
 
-def spectral_reduction(data,dim=2):
-    embedder = manifold.SpectralEmbedding(n_components=dim, random_state=0,
-                                     eigen_solver="arpack")
-    X_prim=embedder.fit_transform(data.X)
-    return X_prim
-
-def pca_reduction(data,dim=2,change=False):
-    embedder = sklearn.decomposition.SparsePCA(n_components=dim)
-    X_prim=embedder.fit_transform(data.X)
-    if(change):
-        data.X=X_prim
-        return data
-    return X_prim
-
-def no_reduction(data,dim=2):
-    return data.X
-
-reductions=[no_reduction,tsne_reduction,lle_reduction,hessian_reduction,spectral_reduction,pca_reduction]
-
-def show_unlabeled(path,reduction_id):
-    data=dataset.csv_to_dataset(path)
-    tsne_X=reductions[reduction_id](data)
-    tsne_data=dataset.Dataset(tsne_X)
-    plot.unlabeled_plot2D(tsne_data)
-
-def show_labeled(path,reduction_id,tabu=[]):
-    data=  dataset.get_annotated_dataset(path)  #dataset.labeled_to_dataset(path)
-    tsne_X= tsne_reduction(data) #reductions[reduction_id](data)
-    tsne_data= dataset.AnnotatedDataset(tsne_X,data.y,data.persons)
-    print(tsne_data.dim)
-    plot.labeled_plot2D(tsne_data,tabu)
-
-def parse_args(args):
-    if(len(sys.argv)==1):
-        reduction_id=0
-    else:    
-        reduction_id=int(sys.argv[1])
-    return reduction_id
+    for i in range(n_points):
+        color_i= color_helper(i,y[i])
+        plt.text(X[i, 0], X[i, 1], str(y[i]),
+                   color=plt.cm.tab20( color_i),
+                   fontdict={'weight': 'bold', 'size': 9})
+    print(x_min,x_max)
+    #plt.xticks(np.arange(x_min, x_max, 0.005)), plt.yticks([])
+    if title is not None:
+        plt.title(title)
+    if(show):
+        plt.show()
+    return plt
 
 if __name__ == "__main__":
-    in_path='../reps/inspect/b_nn/feat.txt'
-    show_labeled(in_path,0)#,[5,6])
-
+    show_feats('datasets/btf.txt')
