5ce6a4914c68500ead514886e95bf22fefaecfd5
tjacek <tjacek@student.agh.edu.pl>
2019-08-28 00:38:53 +0200

    faster selection

--- exper/selection.py
+++ exper/selection.py
@@ -5,21 +5,28 @@ import matplotlib.pyplot as plt
 
 def acc_curve(dict_args):
     quality=exper.persons.quality(**dict_args)
-    acc=[ clf_select(i+1,dict_args,quality) 
-            for i in range(len(quality)-1)]
+    votes=get_votes(dict_args,clf_type="LR")
+    acc=[selected_voting(i+1,quality,votes) 
+            for i in range(len(quality)) ]
+    #acc=[ clf_select(i+1,dict_args,quality) 
+    #        for i in range(len(quality))]
     print(acc)    
     plt.plot(acc)
     plt.ylabel("acc")
     plt.show()
 
 def clf_select(n_clf,args,quality):
-    print(quality)
+    votes=get_votes(args,clf_type="LR")
+    return selected_voting(n_clf,quality,votes)
+    
+def get_votes(args,clf_type="LR"):
     datasets=exper.voting.get_datasets(**args)
-    datasets=[datasets[i] for i in quality[:n_clf]]
-    print("Used datasets")
+    return [exper.predict_labels(data_i,clf_type)  
+                for data_i in datasets]
+
+def selected_voting(n_clf,quality,votes):
     print(quality[:n_clf])
-    votes=[exper.predict_labels(data_i,clf_type="LR")  
-            for data_i in datasets]
-    y_true,y_pred=exper.voting.voting(votes)
+    votes=[votes[i] for i in quality[:n_clf]]
+    y_true,y_pred=exper.voting.count_votes(votes)
     print(classification_report(y_true, y_pred,digits=4))
     return accuracy_score(y_true,y_pred)
