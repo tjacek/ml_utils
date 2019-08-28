232d8a0ca2bf05550186268c5fd56578e3837fc3
tjacek <tjacek@student.agh.edu.pl>
2019-08-28 10:15:14 +0200

    refactore persons

--- exper/persons.py
+++ exper/persons.py
@@ -1,37 +1,26 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import voting,filtr
+import voting,filtr,learn
 
-def quality(**args):
+def quality(args,clf_type="LR"):
     datasets=voting.get_datasets(**args)
-    acc=[ pred_acc(data_i) for data_i in datasets]
+    acc=[ pred_acc(data_i,clf_type) for data_i in datasets]
     print(acc)
     return np.flip(np.argsort(acc))
 
-def pred_acc(data_i):
+def pred_acc(data_i,clf_type="LR"):
     print(data_i.dim())
     train,test=filtr.split(data_i.info)
     train_data=filtr.filtered_dict(train,data_i)
     by_person=samples_by_person(train)
-    def one_out(i):
-        one,rest=by_person[i],[]
-        for person_j,names_j in by_person.items():
-            if(person_j!=i):
-                rest+=names_j
-        return one,rest
+    person_pred=pred_by_person(train_data,by_person,clf_type)
     acc=[]
-    for person_i in by_person.keys():
-        print(person_i)	
-        one,rest=one_out(person_i)
-        X_rest,y_rest=as_arrays(rest,train_data)
-        clf_i=LogisticRegression()
-        clf_i.fit(X_rest,y_rest)
-        X_one,y_one=as_arrays(one,train_data)
-        y_pred=clf_i.predict(X_one)
+    for person_i,pred_i in person_pred.items():
+        one,y_one,y_pred=pred_i
         acc.append(accuracy_score(y_one,y_pred))
     print(acc)
-    return np.mean(acc)
+    return np.min(acc)#np.median(acc)
 
 def samples_by_person(train):
     persons_dict=get_person( train)
@@ -41,6 +30,24 @@ def samples_by_person(train):
                         if(person_j==person_i)]
                             for person_i in person_ids}
     return by_person
+
+def pred_by_person(train_data,by_person,clf_type):
+    def one_out(i):
+        one,rest=by_person[i],[]
+        for person_j,names_j in by_person.items():
+            if(person_j!=i):
+                rest+=names_j
+        return one,rest
+    def pred_helper(person_i):
+        one,rest=one_out(person_i)
+        X_rest,y_rest=as_arrays(rest,train_data)
+        clf_i=learn.get_cls(clf_type)
+        clf_i.fit(X_rest,y_rest)
+        X_one,y_one=as_arrays(one,train_data)
+        y_pred=clf_i.predict(X_one)
+        return (one,y_one,y_pred)
+    acc=[]
+    return { person_i:pred_helper(person_i) for person_i in by_person.keys()}
 
 def get_person(names):
     return { name_i:name_i.split('_')[1]  for name_i in names}
--- exper/selection.py
+++ exper/selection.py
@@ -3,9 +3,9 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
-def acc_curve(dict_args):
-    quality=exper.persons.quality(**dict_args)
-    votes=get_votes(dict_args,clf_type="LR")
+def acc_curve(dict_args,clf_type="LR"):
+    quality=exper.persons.quality(dict_args,clf_type)
+    votes=get_votes(dict_args,clf_type)
     acc=[selected_voting(i+1,quality,votes) 
             for i in range(len(quality)) ]
     #acc=[ clf_select(i+1,dict_args,quality) 
@@ -18,7 +18,7 @@ def acc_curve(dict_args):
 def clf_select(n_clf,args,quality):
     votes=get_votes(args,clf_type="LR")
     return selected_voting(n_clf,quality,votes)
-    
+
 def get_votes(args,clf_type="LR"):
     datasets=exper.voting.get_datasets(**args)
     return [exper.predict_labels(data_i,clf_type)  
