8619382e199dc043f59356e09a7de18749a890c9
tjacek <tjacek@agh.edu.pl>
2019-10-02 12:23:09 +0200

    split votes

--- exper/probs.py
+++ exper/probs.py
@@ -2,11 +2,21 @@
 import exper,exper.voting
 import files,feats
 
-def votes_dist(args,out_path):#,clf_type="LR",out_path=None):    
+def votes_dist(args,out_path,split=True):#,clf_type="LR",out_path=None):    
     datasets=exper.voting.get_datasets(**args)
     files.make_dir(out_path)
+    votes=[]
     for i,data_i in enumerate( datasets):
         model_i=exper.make_model(data_i)
-        dist_i=model_i.predict_proba(data_i.X)
-        vote_dist=feats.FeatureSet(dist_i,data_i.info)
-        vote_dist.save(out_path+'/votes'+str(i))
+        if(split):
+            train,test=exper.split_data(data_i)
+            X,info=test.X,test.info
+        else:
+            X,info=data_i.X,data_i.info
+        dist_i=model_i.predict_proba(X)
+        vote_i=feats.FeatureSet(dist_i,info)
+        if(out_path):
+            out_i=out_path+'/votes'+str(i)
+            vote_i.save(out_i)
+        votes.append(vote_i)
+    return votes    
