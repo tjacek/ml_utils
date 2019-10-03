cdc94bfeb4f1bc84d6632a01e5f08083a977c8dd
tjacek <tjacek@agh.edu.pl>
2019-10-03 12:54:35 +0200

    kld matrix

--- exper/probs.py
+++ exper/probs.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+
 from sklearn.metrics import classification_report
 import exper,exper.voting
 import files,feats,filtr
@@ -56,3 +60,25 @@ def threshold_voting(votes,thres=0.7):
 def simple_voting(votes):
     cats=np.argmax(votes,axis=1)
     return np.argmax(np.bincount(cats))
+
+def show_kld(args,out_path,clf_type="LR"):
+    votes=votes_dist(args,out_path=None,split=True,clf_type=clf_type)
+    vote_dict=as_vote_dict(votes)
+    files.make_dir(out_path)
+    for name_k,vote_i in vote_dict.items():
+        kl_matrix=[[ kl_divergence(p, q) for q in vote_i]
+                        for p in vote_i]
+        out_k=out_path+'/'+name_k
+        heat_map(kl_matrix,out_k)
+
+def kl_divergence(p, q):
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+
+def heat_map(conf_matrix,out_path):
+    conf_matrix=np.around(conf_matrix,2)
+    dim=conf_matrix.shape
+    df_cm = pd.DataFrame(conf_matrix, range(dim[0]),range(dim[1]))
+    sn.set(font_scale=1.0)#for label size
+    sn.heatmap(df_cm, annot=True,annot_kws={"size": 6}, fmt='g')
+    plt.savefig(out_path)
+    plt.clf()
