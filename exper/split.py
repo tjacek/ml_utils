13c8ec0b3cace0b14ca83251ab779ffcf1aa88b7
tjacek <tjacek@agh.edu.pl>
2019-10-05 23:28:55 +0200

    clean split module

--- exper/split.py
+++ exper/split.py
@@ -1,10 +1,6 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sets import Set
 import filtr,feats,exper.voting
-
-def exper_voting(in_path,restr,args):
-    datasets=exper.voting.get_datasets(**args)
 
 def exper_single(args,restr,clf_type="SVC"):
     datasets=exper.voting.get_datasets(**args)
@@ -17,12 +13,9 @@ def restr_voting(datasets,restr,clf_type="LR"):
     subset=[by_cats(data_i,restr) for data_i in datasets] 
     preds=[exper.predict_labels(subset_i,clf_type) for subset_i in subset]
     return exper.voting.count_votes(preds)
-#def rest_pred(feat_dataset,restr_j,clf_type="LR"):
-#    subset=[by_cats(feat_dataset,restr_j) for restr_j in restr] 
-#    return [predict_labels(subset_i,clf_type) for subset_i in subset]
 
 def by_cats(feat_set,cat_set):
-    cat_set=Set(cat_set)
+    cat_set=set(cat_set)
     def cat_selector(name_i):
         cat_i=int(name_i.split('_')[0])
         return (cat_i in cat_set)
