695acebb216fe3ea72d83fff529965f2056b07b1
tjacek <tjacek@agh.edu.pl>
2019-10-08 19:21:05 +0200

    refactore persons

--- exper/cats.py
+++ exper/cats.py
@@ -1,18 +1,39 @@
 import numpy as np
-#from scipy.stats import mode
+import math
 import exper,exper.persons,feats,files
 
-def adaptive_voting(args,out_path,clf_type="LR"):
+def adaptive_votes(votes_path):
+    votes=feats.read(votes_path)
+#    votes=binarize(votes)
+    exper.exper_single(votes,clf_type="SVC",norm=False)
+
+def make_votes(args,out_path,clf_type="LR"):
     datasets=exper.voting.get_datasets(**args)
     files.make_dir(out_path)
     for i,data_i in enumerate(datasets):
         train_i,test_i=data_i.split()
-        train_votes=exper.persons.pred_by_person(train_i)
+        train_votes=exper.persons.pred_by_person(train_i,clf_type)
         test_votes=exper.persons.pred_vectors(train_i,test_i,clf_type)
-        votes_dict=dict(train_votes+train_votes)
+        votes_dict=dict(train_votes+test_votes)
         votes_feats=feats.from_dict(votes_dict)
         out_i=out_path+'/nn'+str(i)
         votes_feats.save(out_i)
+
+def binarize(votes):
+    n_cats=len(set(votes.get_labels()))
+    n_clfs=int(votes.X.shape[1]/n_cats)
+    X_parts=[votes.X[:,i*n_cats:(i+1)*n_cats]for i in range(n_clfs)]
+    binary_X=[np.array([ one_hot(dist_i)
+                for dist_i in x_j]) 
+                    for x_j in X_parts]
+    binary_X=np.concatenate(binary_X,axis=1)
+    return feats.FeatureSet(binary_X,votes.info)
+
+def one_hot(dist_i):
+    k=np.argmax(dist_i)
+    one_hot_i=np.zeros(dist_i.shape)
+    one_hot_i[k]=1
+    return one_hot_i
 #def adaptive_voting(args,clf_type="LR",out_path=None):    
 #    datasets=exper.voting.get_datasets(**args)
 #    train_votes=get_train_votes(datasets,clf_type)
--- exper/persons.py
+++ exper/persons.py
@@ -12,7 +12,7 @@ def pred_by_person(train_i,clf_type="LR"):
         clf_j=learn.get_cls(clf_type)
         clf_j.fit(rest_j.X,rest_j.get_labels())
         dist_j=clf_j.predict_proba(one_j.X)
-        pairs+=pred_vectors(rest_j,one_j,clf_type)#list(zip(one_j.info,dist_j))
+        pairs+=pred_vectors(rest_j,one_j,clf_type)
     return pairs
 
 def one_out_person(train_i):
@@ -31,52 +31,9 @@ def pred_vectors(train_j,test_j,clf_type="LR"):
     dist_j=clf_j.predict_proba(test_j.X)
     return list(zip(test_j.info,dist_j))
 
-#def in_sample(data_i,clf_type="LR"):
-#    train,test=filtr.split(data_i.info)
-#    train_data=filtr.filtered_dict(train,data_i)
-#    by_person=samples_by_person(train)
-#    person_pred=pred_by_person(train_data,by_person,clf_type)
-#    pairs=[]
-#    for person_i,pred_i in person_pred.items():
-#        one,y_one,y_pred=pred_i
-#        pairs+=zip(one,y_pred)
-#    return pairs
-
-#def samples_by_person(train):
-#    persons_dict=get_person( train)
-#    person_ids=  set(persons_dict.values())#list(np.unique(persons_dict.values()))
-#    by_person={person_i:
-#                    [name_j for name_j,person_j in persons_dict.items()
-#                        if(person_j==person_i)]
-#                            for person_i in person_ids}
-#    return by_person
-
-#def pred_by_person(train_data,by_person,clf_type):
-#    def one_out(i):
-#        one,rest=by_person[i],[]
-#        for person_j,names_j in by_person.items():
-#            if(int(person_j)!=i):
-#                rest+=names_j
-#        return one,rest
-#    def pred_helper(person_i):
-#        one,rest=one_out(person_i)
-#        X_rest,y_rest=as_arrays(rest,train_data)
-#        clf_i=learn.get_cls(clf_type)
-#        clf_i.fit(X_rest,y_rest)
-#        X_one,y_one=as_arrays(one,train_data)
-#        y_pred=clf_i.predict(X_one)
-#        return (one,y_one,y_pred)
-#    acc=[]
-#    return { person_i:pred_helper(person_i) for person_i in by_person.keys()}
-
 def unique_persons(data_i):
     persons_dict=get_person(data_i.info)
     return list(set(persons_dict.values()))
 
 def get_person(names):
     return { name_i:name_i.split('_')[1]  for name_i in names}
-
-#def as_arrays(names,data_i):
-#    X=np.array([data_i[name_i] for name_i in names])
-#    y=[int(name_i.split('_')[0]) -1 for name_i in names]
-#    return X,y
