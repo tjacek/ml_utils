41cf7588941f779d8e15dfbc3b0b4b71ed2aafa2
tjacek <tjacek@agh.edu.pl>
2019-10-09 21:31:21 +0200

    pred_corl

--- exper/inspect.py
+++ exper/inspect.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sn
+import feats
 import matplotlib.pyplot as plt
 import exper.probs as probs ,files
 
@@ -28,6 +29,22 @@ def heat_map(conf_matrix,out_path):
     dim=conf_matrix.shape
     df_cm = pd.DataFrame(conf_matrix, range(dim[0]),range(dim[1]))
     sn.set(font_scale=1.0)#for label size
-    sn.heatmap(df_cm, annot=True,annot_kws={"size": 6}, fmt='g')
-    plt.savefig(out_path)
+    sn.heatmap(df_cm, annot=True,annot_kws={"size": 4}, fmt='g')
+    plt.savefig(out_path,dpi=2000)
     plt.clf()
+
+def pred_corl(in_path,out_path,test=True):
+    datasets=feats.read_list(in_path)
+    files.make_dir(out_path)
+    for i,data_i in enumerate(datasets):
+        data_i=data_i.split()[int(test)]
+        y_i=to_signal(data_i.get_labels())
+        corl=[[ np.corrcoef(x_j, y_j)[1][0]
+                    for x_j in data_i.X.T]
+                        for y_j in y_i]
+        heat_map(corl,out_path+'/nn'+str(i))
+
+def to_signal(y):
+    y=np.array(y)
+    n_cats=max(y)
+    return [ (y==i+1).astype(float) for i in range(n_cats)]
