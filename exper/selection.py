9ac84e7630918a80689fb9501a3243a6641a0d5e
tjacek <tjacek@agh.edu.pl>
2019-10-12 13:37:59 +0200

    full_corls

--- exper/selection.py
+++ exper/selection.py
@@ -7,22 +7,52 @@ from exper.inspect import heat_map
 
 def clf_selection(in_path):
     datasets=[data_i.split()[0] for data_i in feats.read_list(in_path)]
-    clf_corls=[ np.concatenate([dataset_corl(data_i,data_j) 
+    datasets=[residuals(data_i) for data_i in datasets]
+    corl_matrix=full_corls_matrix(datasets)
+    return np.argsort( np.mean(corl_matrix,axis=0))
+#    np.fill_diagonal(kl_matrix, 0)
+#    pairs=np.array(np.where(kl_matrix>0.7))
+#    return from_pairs(pairs)
+#    exper.inspect.heat_map( (kl_matrix>0.7).astype(int) )
+
+def cat_corls_matrix(datasets):
+    clf_corls=[ np.concatenate([cat_corl(data_i,data_j) 
                      for data_i in datasets])
                         for data_j in datasets]
     n_clf=len(clf_corls)
-    kl_matrix=[[np.corrcoef(clf_corls[i], clf_corls[j])[0][1]
+    corl_matrix=[[np.corrcoef(clf_corls[i], clf_corls[j])[0][1]
                     for j in range(n_clf)]
                         for i in range(n_clf)]
-    kl_matrix=np.array(kl_matrix)
-    np.fill_diagonal(kl_matrix, 0)
-    pairs=np.array(np.where(kl_matrix>0.7))
-    return from_pairs(pairs)
-#    exper.inspect.heat_map( (kl_matrix>0.7).astype(int) )
+    return np.array(corl_matrix)
 
-def dataset_corl(data1,data2):
+def cat_corl(data1,data2):
     return [np.corrcoef(x1_i,x2_i)[0][1] 
                 for x1_i,x2_i in zip(data1.X.T,data2.X.T)]
+
+def full_corls_matrix(datasets):
+    clf_matrix=[[np.corrcoef(data_i.X.flatten(),data_j.X.flatten())[0][1]
+                     for data_i in datasets]
+                        for data_j in datasets]
+    return np.array(clf_matrix)
+
+def residuals( data_i):
+    res_i=data_i.labels_array().astype(float)-data_i.X
+    return feats.FeatureSet(res_i,data_i.info)
+
+def kl_divergence(p, q):
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+
+def find_hard_cats(corl_matrix):
+    cat_quality= np.mean(corl_matrix,axis=0)
+    print(cat_quality)
+    cat_quality-=np.mean(cat_quality)
+    cat_quality/=np.std(cat_quality)
+    return (cat_quality<-1)
+
+def find_outliners(arr,neg=True):
+    norm_arr= (arr-np.mean(arr))/np.std(arr)
+    bool_arr= norm_arr<-1 if(neg) else norm_arr>1
+    return np.where(bool_arr)
 
 def from_pairs(pairs):
     values,counts = np.unique(pairs,return_counts=True)
@@ -40,51 +70,3 @@ def from_pairs(pairs):
     wrong=set(values).difference(set(right))
     print(wrong)
     return wrong
-
-def kl_divergence(p, q):
-    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
-
-#def clf_selection(in_path):
-#    corl_train=exper.inspect.all_pred_corl(in_path,None,test=False)
-#    corl_test=exper.inspect.all_pred_corl(in_path,None,test=True)
-#    diff_corl= corl_test-corl_train
-#    print(find_outliners(np.mean(diff_corl,axis=0),neg=True))    
-#    exper.inspect.heat_map(diff_corl).show()  
-
-#def __clf_selection(in_path):
-#    corl_matrix=exper.inspect.all_pred_corl(in_path,None,test=True)
-#    corl_matrix-= np.expand_dims(np.mean(corl_matrix,axis=0),axis=1)
-#    corl_matrix/=np.expand_dims(np.std(corl_matrix,axis=0),axis=1)
-    
-#    better=(corl_matrix>1).astype(int)
-#    clf_quality=np.sum(better,axis=1)
-#    clf_quality= (clf_quality -np.mean(clf_quality))/np.std(clf_quality)   
-#    print(clf_quality)
-#    print(np.where(clf_quality>1))
-
-#def _clf_selection(in_path):
-#    corl_matrix=exper.inspect.all_pred_corl(in_path,None,test=False)
-#    hard_cats=find_hard_cats(corl_matrix)
-#    hard_matrix=corl_matrix[:,hard_cats]
-#    hard_matrix-= np.expand_dims(np.mean(hard_matrix,axis=1),axis=1)
-#    hard_matrix/=np.expand_dims(np.std(hard_matrix,axis=1),axis=1)
-    
-#    hard_matrix[hard_matrix>-1]=0
-#    hard_matrix[hard_matrix<-1]=1
-
-#    heat_map(hard_matrix).show()
-#    clf_quality=np.sum(hard_matrix,axis=1)
-#    print(clf_quality)
-#    print(np.where(clf_quality>1))
-
-def find_hard_cats(corl_matrix):
-    cat_quality= np.mean(corl_matrix,axis=0)
-    print(cat_quality)
-    cat_quality-=np.mean(cat_quality)
-    cat_quality/=np.std(cat_quality)
-    return (cat_quality<-1)
-
-def find_outliners(arr,neg=True):
-    norm_arr= (arr-np.mean(arr))/np.std(arr)
-    bool_arr= norm_arr<-1 if(neg) else norm_arr>1
-    return np.where(bool_arr)
