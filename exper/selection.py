92d8bb716c13e941b34c483e77ae62a3e216dbd8
tjacek <tjacek@agh.edu.pl>
2019-10-16 10:42:38 +0200

    find outliners

--- exper/selection.py
+++ exper/selection.py
@@ -9,11 +9,18 @@ def clf_selection(in_path):
     datasets=[data_i.split()[0] for data_i in feats.read_list(in_path)]
     datasets=[residuals(data_i) for data_i in datasets]
     corl_matrix=full_corls_matrix(datasets)
-    return np.argsort( np.mean(corl_matrix,axis=0))
-#    np.fill_diagonal(kl_matrix, 0)
-#    pairs=np.array(np.where(kl_matrix>0.7))
-#    return from_pairs(pairs)
-#    exper.inspect.heat_map( (kl_matrix>0.7).astype(int) )
+    mcc= np.mean(corl_matrix,axis=0)
+    return np.argsort(mcc)#find_outliners(mcc,False)
+    
+def full_corls_matrix(datasets):
+    clf_matrix=[[np.corrcoef(data_i.X.flatten(),data_j.X.flatten())[0][1]
+                     for data_i in datasets]
+                        for data_j in datasets]
+    return np.array(clf_matrix)
+
+def residuals( data_i):
+    res_i=data_i.labels_array().astype(float)-data_i.X
+    return feats.FeatureSet(res_i,data_i.info)
 
 def cat_corls_matrix(datasets):
     clf_corls=[ np.concatenate([cat_corl(data_i,data_j) 
@@ -29,15 +36,10 @@ def cat_corl(data1,data2):
     return [np.corrcoef(x1_i,x2_i)[0][1] 
                 for x1_i,x2_i in zip(data1.X.T,data2.X.T)]
 
-def full_corls_matrix(datasets):
-    clf_matrix=[[np.corrcoef(data_i.X.flatten(),data_j.X.flatten())[0][1]
-                     for data_i in datasets]
-                        for data_j in datasets]
-    return np.array(clf_matrix)
-
-def residuals( data_i):
-    res_i=data_i.labels_array().astype(float)-data_i.X
-    return feats.FeatureSet(res_i,data_i.info)
+def find_outliners(samples,pos=True):
+    norm_samples= (samples-np.mean(samples))/np.std(samples)
+    indexes= (norm_samples<1.0) if(pos) else (norm_samples>1.0)
+    return np.where(indexes)[0]
 
 def from_pairs(pairs):
     values,counts = np.unique(pairs,return_counts=True)
