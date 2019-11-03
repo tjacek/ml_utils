d4f58d0526a30f787ffc7c07da17f32b5056a5dc
tjacek <tjacek@agh.edu.pl>
2019-11-03 15:29:33 +0100

    subspace dataset

--- bag.py
+++ bag.py
@@ -1,16 +1,21 @@
+import numpy as np
 import random,os.path
 import unify,filtr,dataset,files
 
 def bag_ens(in_path,size=6):
-    cur_dir=os.path.split(in_path)[0]
-    raw_ts=unify.read(in_path)
-    out_path=cur_dir+'/bag'
-    files.make_dir(out_path)
+    out_path,raw_ts=prepare_date(in_path,name='/bag')
     for k in range(size):
         bag_k=sampled_dataset(raw_ts)
         out_k=out_path+'/sample'+str(k)
         bag_k.save(out_k)
     raw_ts.save(out_path+'/full')
+
+def prepare_date(in_path,name='/bag'):
+    cur_dir=os.path.split(in_path)[0]
+    raw_ts=unify.read(in_path)
+    out_path=cur_dir+name
+    files.make_dir(out_path)
+    return out_path,raw_ts
 
 def sampled_dataset(ts_dataset):
     train,test=filtr.split(ts_dataset.ts_names())
@@ -20,4 +25,20 @@ def sampled_dataset(ts_dataset):
     pairs+=[ (name_i,ts_dataset[name_i]) for name_i in test]
     return dataset.TSDataset(dict(pairs) ,ts_dataset.name+'_bag')
 
-bag_ens("../MSR/agum")
+def jackknife(in_path):
+    out_path,raw_ts=prepare_date(in_path,name='/jack')
+    for i in range(raw_ts.n_feats()):
+        subspace_dataset(i,raw_ts)
+
+def subspace_dataset(k,ts_dataset):
+    names=ts_dataset.ts_names()
+    sub_dict={}
+    for name_i in names:
+        feats_i=ts_dataset.as_features(name_i)
+        s_feats_i=[ts_j 
+                    for j,ts_j in enumerate(feats_i)
+                        if(j!=k)]
+        sub_dict[name_i]=np.array(s_feats_i)
+    return dataset.TSDataset(sub_dict,ts_dataset.name+'_sub')
+
+jackknife("../MSR/agum")
