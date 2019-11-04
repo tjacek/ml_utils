8355f3e318cb224edd5acb0b411151cacf686249
tjacek <tjacek@agh.edu.pl>
2019-11-04 16:17:57 +0100

    split ensemble

--- exper/split.py
+++ exper/split.py
@@ -1,18 +1,39 @@
 import numpy as np
-from sklearn.metrics import accuracy_score
-import filtr,feats,exper.voting
+import exper,exper.persons
+import feats,filtr
 
-def exper_single(args,restr,clf_type="SVC"):
+def split_ensemble(args,clf_type="LR",restr=None):
     datasets=exper.voting.get_datasets(**args)
-    result=[restr_voting(datasets,restr_i,clf_type) for restr_i in restr]
-    acc=[accuracy_score(result_i[0],result_i[1]) for result_i in result]
-    mean_acc=np.mean(acc)
-    return acc,mean_acc
+    datasets=restr_dataset(datasets,restr)
+    print(len(datasets))
+    acc=[train_cls(list(data_i),clf_type) for data_i in datasets]
+    print(acc)
+    print(np.mean(acc))
 
-def restr_voting(datasets,restr,clf_type="LR"):
-    subset=[by_cats(data_i,restr) for data_i in datasets] 
-    preds=[exper.predict_labels(subset_i,clf_type) for subset_i in subset]
-    return exper.voting.count_votes(preds)
+def train_cls(datasets,clf_type="SVC"):
+    results=[]
+    for data_i in datasets:
+        train,test=data_i.split()
+        pairs=exper.persons.pred_vectors(train,test,clf_type)
+        results.append(dict(pairs))
+    names=results[0].keys()
+    votes={name_i: [result_i[name_i] for result_i in results]
+                for name_i in names}
+    pred=[ correc_pred(name_i,vote_i)
+            for name_i,vote_i in votes.items()]
+    return np.mean(pred)
+
+def correc_pred(name_i,votes):
+    cat_i=int(name_i.split('_')[0])-1
+    dist=np.sum(votes,axis=0)
+    pred_i=np.argmax(dist)
+    return float(cat_i==pred_i)
+
+def restr_dataset(datasets,restr):
+    subset=[[by_cats(data_i,restr_j) 
+                for restr_j in restr]
+                    for data_i in datasets] 
+    return list(zip(*subset))
 
 def by_cats(feat_set,cat_set):
     cat_set=set(cat_set)
@@ -21,4 +42,5 @@ def by_cats(feat_set,cat_set):
         return (cat_i in cat_set)
     train,test=filtr.split(feat_set.info,cat_selector)
     cat_dict=filtr.filtered_dict(train,feat_set.to_dict())
+    cat_dict=filtr.ordered_cats(cat_dict)
     return feats.from_dict(cat_dict)
