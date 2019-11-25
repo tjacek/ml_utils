69028436bff25dd6ae14201030ce7a7691e43a19
tjacek <tjacek@agh.edu.pl>
2019-11-25 22:10:34 +0100

    voting weights

--- exper/inspect.py
+++ exper/inspect.py
@@ -19,24 +19,14 @@ def pred(data_i):
     y_true=data_i.get_labels()
     return y_true,y_pred
 
-#def show_errors(args,out_path,clf_type="LR"):
-#    def helper(name_i,votes):
-#        true_cat=files.natural_keys(name_i)[1]-1
-#        pred_cat=probs.simple_voting(votes)
-#        return true_cat!=pred_cat
-#    show_probs(args,out_path,clf_type="LR",selector=helper)
-
-#def show_probs(args,out_path,clf_type="LR",selector=None):
-#    votes=probs.votes_dist(args,out_path=None,split=True,clf_type=clf_type)
-#    vote_dict=probs.as_vote_dict(votes)
-#    files.make_dir(out_path)
-#    if(not selector):
-#        selector=lambda x,y:True
-#    for name_k,vote_i in vote_dict.items():
-#        if(selector(name_k,vote_i)):
-#            kl_matrix=np.array(vote_i)
-#            out_k=out_path+'/'+name_k
-#            heat_map(kl_matrix,out_k)
+def show_votes(votes_path,out_path=None):
+    votes=feats.read_list(votes_path)
+    full_X=np.array([vote_i.X.T for vote_i in votes]).T
+    weights=np.mean(full_X,axis=2)
+    votes=feats.FeatureSet(weights,votes[0].info)
+    if(out_path):
+        votes.save(out_path)
+    return accuracy_score(*pred(votes))
 
 def heat_map(conf_matrix,out_path):
     conf_matrix=np.around(conf_matrix,2)
