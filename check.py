915d752e50584572b7a0c772108795b6ff000cb4
tjacek <tjacek@agh.edu.pl>
2019-11-27 23:22:20 +0100

    check used features

--- check.py
+++ check.py
@@ -1,8 +1,26 @@
 import numpy as np
-import feats
+import files,dataset
+#import exper.inspect,
 
 def sparse_feats(in_path):
-    data=feats.read(in_path)
-    weight=np.sum(data.X,axis=0)
-    weight/=np.std(weight)
-    print(weight)
+    seqs=[ dataset.read_dataset(path_i) 
+            for path_i in files.top_files(in_path)]
+    feat=[np.sum(np.abs(seq_i.to_array()),axis=0)
+            for seq_i in seqs]
+    for feat_i in feat:
+        threshold(feat_i)
+
+def threshold(feat_i):
+    feat_i[feat_i<1]=0.0
+    feat_i[ feat_i>1]=1.0
+    print(np.sum(feat_i))
+
+def std_threshold(feat_i):
+    feat_i=norm(np.sort(feat_i))
+    print(feat_i)
+
+def norm(arr):
+    arr-=np.mean(arr)
+    return arr/np.std(arr)
+
+sparse_feats("../old/single_smooth/binary_seq")
