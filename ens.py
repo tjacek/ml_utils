4820421dc7f85892233166b9d974b2dd440221ce
tjacek <tjacek@agh.edu.pl>
2020-01-15 12:56:02 +0100

    ens exp

--- ens.py
+++ ens.py
@@ -1,5 +1,12 @@
 import exper.cats,exper.selection,exper.inspect
 
+def exp(args,in_path,dir_path=None,clf="LR",train=True):
+    if(dir_path):
+    	in_path+="/"+dir_path
+    if(train):
+        exper.cats.make_votes(args,in_path,clf_type=clf)
+    exper.cats.adaptive_votes(in_path)
+
 def show_acc_curve(in_path,dir_path=None):
     if(dir_path):
         in_path+="/"+dir_path
