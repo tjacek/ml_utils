4a969c48620aa095e51c7552e09223a6df8451c3
tjacek <tjacek@agh.edu.pl>
2020-02-05 23:51:58 +0100

    show single selection result

--- ens.py
+++ ens.py
@@ -1,4 +1,5 @@
-import exper.cats,exper.selection,exper.inspect
+import exper.cats,exper.selection,exper.inspect,learn
+from sklearn.metrics import classification_report,accuracy_score
 
 def exp(args,in_path,dir_path=None,clf="LR",train=True):
     if(dir_path):
@@ -7,13 +8,20 @@ def exp(args,in_path,dir_path=None,clf="LR",train=True):
         exper.cats.make_votes(args,in_path,clf_type=clf)
     exper.cats.adaptive_votes(in_path)
 
-def show_acc_curve(in_path,dir_path=None):
+def show_acc_curve(in_path,dir_path=None,n_select=None):
     if(dir_path):
         in_path+="/"+dir_path
     ord=exper.selection.clf_selection(in_path)
-    acc=exper.cats.acc_curve(in_path,ord)
+    return exper.cats.acc_curve(in_path,ord)
 
 def show_acc(in_path,dir_path=None):
     if(dir_path):
         in_path+="/"+dir_path
     print(exper.inspect.clf_acc(in_path))
+
+def selection_result(vote_path,n_select):
+    ord=exper.selection.clf_selection(vote_path)
+    votes=exper.cats.selected_votes(vote_path,ord,binary=False)
+    s_votes=votes[:n_select]
+    result=exper.cats.simple_voting(votes)
+    print(classification_report(result[1], result[0],digits=4))
--- exper/cats.py
+++ exper/cats.py
@@ -70,16 +70,20 @@ def adaptive_exp(votes_path,out_path=None):
     file_str.close()
 
 def acc_curve(vote_path,ord,binary=False):
-    votes=feats.read_list(vote_path)
-    if(binary):
-        votes=[binarize(vote_i) for vote_i in votes]
-    votes=[ votes[k] for k in ord]
+    votes=selected_votes(vote_path,ord,binary)
     n_clf=len(votes)
     results=[voting(votes[:(i+1)],None) for i in range(n_clf)]
     acc=[ learn.compute_score(result_i[0],result_i[1],False)[0] 
             for result_i in results]
-    show_curve(acc,n_clf,vote_path,binary)
+    show_curve(acc,len(results),vote_path,binary)
     return acc
+
+def selected_votes(vote_path,ord,binary=False):
+    votes=feats.read_list(vote_path)
+    if(binary):
+        votes=[binarize(vote_i) for vote_i in votes]
+    return [ votes[k] for k in ord]
+    
 
 def show_curve(acc,n_clf,vote_path,binary):
     title="_".join(vote_path.split('/'))
