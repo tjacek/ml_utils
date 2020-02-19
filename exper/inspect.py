1e737b958e43f91e88d6d26d86d5c1b6893b89e7
tjacek <tjacek@agh.edu.pl>
2020-02-19 22:26:56 +0100

    clf_acc improved

--- exper/inspect.py
+++ exper/inspect.py
@@ -1,16 +1,15 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-#import pandas as pd
-#import seaborn as sn
-#import matplotlib.pyplot as plt
-#import exper.probs as probs ,files
-#import exper,exper.voting,learn
 import feats
 import exper.cats  
 
-def clf_acc(votes_path):
+def clf_acc(votes_path,data="test"):
     votes=feats.read_list(votes_path)
+    if(data=="test"):
+        votes=[ vote_i.split()[1] for vote_i in votes]
+    if(data=="train"):
+        votes=[ vote_i.split()[0] for vote_i in votes]
     result=[ pred(vote_i) for vote_i in votes]
     return [accuracy_score(*result_i) for result_i in result]
 
@@ -28,36 +27,14 @@ def show_votes(votes_path,out_path=None):
         votes.save(out_path)
     return accuracy_score(*pred(votes))
 
-def heat_map(conf_matrix,out_path):
-    conf_matrix=np.around(conf_matrix,2)
-    dim=conf_matrix.shape
-    df_cm = pd.DataFrame(conf_matrix, range(dim[0]),range(dim[1]))
-    sn.set(font_scale=1.0)#for label size
-    sn.heatmap(df_cm, annot=True,annot_kws={"size": 4}, fmt='g')
-    plt.savefig(out_path,dpi=2000)
-    plt.clf()
-
-#def pred_corl(in_path,out_path,test=True):
-#    datasets=feats.read_list(in_path)
-#    files.make_dir(out_path)
-#    for i,data_i in enumerate(datasets):
-#        data_i=data_i.split()[int(test)]
-#        y_i=to_signal(data_i.get_labels())
-#        corl=[[ np.corrcoef(x_j, y_j)[1][0]
-#                    for x_j in data_i.X.T]
-#                        for y_j in y_i]
-#        heat_map(corl,out_path+'/nn'+str(i))
-
-#def all_pred_corl(in_path,out_path,test=True):
-#    datasets=feats.read_list(in_path)
-#    corl_matrix=[]
-#    for i,data_i in enumerate(datasets):
-#        data_i=data_i.split()[int(test)]
-#        y_i=to_signal(data_i.get_labels())
-#        corl=[ np.corrcoef(x_j, y_i[j])[1][0]
-#                    for j,x_j in enumerate(data_i.X.T)]
-#        corl_matrix.append(corl)            
-#    heat_map(np.array(corl_matrix),out_path)
+#def heat_map(conf_matrix,out_path):
+#    conf_matrix=np.around(conf_matrix,2)
+#    dim=conf_matrix.shape
+#    df_cm = pd.DataFrame(conf_matrix, range(dim[0]),range(dim[1]))
+#    sn.set(font_scale=1.0)#for label size
+#    sn.heatmap(df_cm, annot=True,annot_kws={"size": 4}, fmt='g')
+#    plt.savefig(out_path,dpi=2000)
+#    plt.clf()
 
 def to_signal(y):
     y=np.array(y)
--- exper/selection.py
+++ exper/selection.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 import exper,exper.cats,filtr,feats
 import exper.inspect
 
-from exper.inspect import heat_map
+#from exper.inspect import heat_map
 
 def clf_selection(in_path):
-    datasets=[data_i.split()[0] for data_i in feats.read_list(in_path)]
-    datasets=[residuals(data_i) for data_i in datasets]
-    corl_matrix=full_corls_matrix(datasets)
+    corl_matrix=get_res_corelation(in_path)
     mcc= np.mean(corl_matrix,axis=0)
+    print(mcc)
     return np.argsort(mcc)#find_outliners(mcc,False)
+
+def get_res_corelation(in_path):
+    datasets=[data_i.split()[1] for data_i in feats.read_list(in_path)]
+    datasets=[errors(data_i) for data_i in datasets]
+    return full_corls_matrix(datasets)
     
 def full_corls_matrix(datasets):
     clf_matrix=[[np.corrcoef(data_i.X.flatten(),data_j.X.flatten())[0][1]
@@ -18,6 +22,14 @@ def full_corls_matrix(datasets):
                         for data_j in datasets]
     return np.array(clf_matrix)
 
+def errors(data_i):
+    y_true=data_i.get_labels()
+    y_pred=[np.argmax(x_i) for x_i in data_i.X] 
+    error=[ float(true_j!=pred_j) 
+                for true_j,pred_j in zip(y_true,y_pred)]
+    return feats.FeatureSet(np.array(error),data_i.info)
+
 def residuals( data_i):
-    res_i=data_i.labels_array().astype(float)-data_i.X
+    res_i=np.abs(data_i.labels_array().astype(float)-data_i.X)
     return feats.FeatureSet(res_i,data_i.info)
+
