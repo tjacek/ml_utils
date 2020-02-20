0620bcb73c57be6e19c60bb88a24a1604d149e4e
tjacek <tjacek@agh.edu.pl>
2020-02-20 01:42:52 +0100

    inspect

--- exper/inspect.py
+++ exper/inspect.py
@@ -1,11 +1,10 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
-
-import feats
+import feats,learn,files
 import exper.cats  
 
 def clf_acc(votes_path,data="test"):
-    votes=read_votes(votes_path)
+    votes=read_votes(votes_path,data)
     result=[ pred(vote_i) for vote_i in votes]
     return [accuracy_score(*result_i) for result_i in result]
 
@@ -30,23 +29,13 @@ def pred(data_i):
     y_true=data_i.get_labels()
     return y_true,y_pred
 
-def show_votes(votes_path,out_path=None):
-    votes=feats.read_list(votes_path)
-    full_X=np.array([vote_i.X.T for vote_i in votes]).T
-    weights=np.mean(full_X,axis=2)
-    votes=feats.FeatureSet(weights,votes[0].info)
-    if(out_path):
-        votes.save(out_path)
-    return accuracy_score(*pred(votes))
-
-#def heat_map(conf_matrix,out_path):
-#    conf_matrix=np.around(conf_matrix,2)
-#    dim=conf_matrix.shape
-#    df_cm = pd.DataFrame(conf_matrix, range(dim[0]),range(dim[1]))
-#    sn.set(font_scale=1.0)#for label size
-#    sn.heatmap(df_cm, annot=True,annot_kws={"size": 4}, fmt='g')
-#    plt.savefig(out_path,dpi=2000)
-#    plt.clf()
+def show_votes(votes_path,out_path,data="train"):
+    votes=read_votes(votes_path,data=data)
+    files.make_dir(out_path)
+    for i,vote_i in enumerate(votes):
+        out_i="%s/nn%d" % (out_path,i)
+        result_i=pred(vote_i)
+        learn.show_confusion(result_i,out_i) 
 
 def binary_result(y_true,y_pred):
     return np.array([float(true_i==pred_i) 
