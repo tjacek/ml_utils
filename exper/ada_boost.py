1d87288057a8d5f9c8c67aa31f6653a864e2739a
tjacek <tjacek@agh.edu.pl>
2020-02-24 21:27:17 +0100

    fix ada boost

--- exper/ada_boost.py
+++ exper/ada_boost.py
@@ -17,32 +17,33 @@ def samme(train):
     K=train[0].n_cats()
     alphas=np.zeros((K,))
     weights=init_weigts(train[0])
+    active_cls=np.zeros((K,))
     for i in range(len(train)):
-        all_errors=[error(train_i,weights)
-                        for train_i in train]
-        active_cls=1-alphas
-        active_cls[active_cls<1.0]=0.0
-        all_errors= all_errors*active_cls
-        t=np.argmax(all_errors)
+        all_errors=np.array([error(train_i,weights)
+                                    for train_i in train])
+        all_errors[active_cls>0.0]=np.inf
+        t=np.argmin(all_errors)
+        active_cls[t]=1.0
         err_t=all_errors[t]
         if(err_t<0.01):
             err_t=0.01
-        alphas[t]=np.log( (1.0-err_t)/err_t)+np.log(K-1)
+        alphas[t]=np.log((1.0-err_t)/err_t)+np.log(K-1)
         weights=recompute_weights(train[t],alphas[t],weights)
+    print(alphas)
     return alphas
 
 def error(train_i,weights):
 	X,y=train_i.X,train_i.get_labels()
 	return sum([weights[j]*(1.0-x_i[y[j]]) for j,x_i in enumerate(X)])
 
-def init_weigts(train):
-    weights=np.ones((len(train),))
+def init_weigts(train_i):
+    weights=np.ones((len(train_i),))
     weights/=weights.shape[0]
     return weights
 
 def recompute_weights(data_t,alpha_t,weights):
     X,y=data_t.X,data_t.get_labels()
-    weights=[ weight_j* np.exp( alpha_t* data_t.X[j,y[j]]) 
+    weights=[ weight_j* np.exp( alpha_t* (1.0- data_t.X[j,y[j]])) 
                 for j,weight_j in enumerate(weights)]
     weights=np.array(weights)
     return weights/np.sum(weights)
