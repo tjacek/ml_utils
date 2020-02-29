8dcca471ab474795ff6e6c2040662dca340d3f80
tjacek <tjacek@agh.edu.pl>
2020-02-29 23:23:01 +0100

    title

--- exper/curve.py
+++ exper/curve.py
@@ -9,7 +9,11 @@ def acc_curve(vote_path,ord,binary=False,show=True):
     acc=[ learn.compute_score(result_i[0],result_i[1],False)[0] 
             for result_i in results]
     if(show):
-        show_curve(acc,len(results),vote_path,binary)
+        title="_".join(vote_path.split('/'))
+        title=title.replace(".._","").replace("votes","")
+        voting_type= "HARD" if(binary) else "SOFT"
+        title+=voting_type
+        show_curve(acc,len(results),title)
     return acc
 
 def selected_votes(vote_path,ord,binary=False):
@@ -18,11 +22,7 @@ def selected_votes(vote_path,ord,binary=False):
         votes=[binarize(vote_i) for vote_i in votes]
     return [ votes[k] for k in ord]
     
-def show_curve(acc,n_clf,vote_path,binary):
-    title="_".join(vote_path.split('/'))
-    title=title.replace(".._","").replace("votes","")
-    voting_type= "HARD" if(binary) else "SOFT"
-    title+=voting_type
+def show_curve(acc,n_clf,vote_path):
     plt.title(title)
     plt.grid(True)
     plt.xlabel('number of classifiers')
