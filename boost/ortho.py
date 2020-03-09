1429884e8fbfc1286e2e8330c0c56099dd68b08b
tjacek <tjacek@agh.edu.pl>
2020-03-09 22:49:56 +0100

    person acc experiment

--- boost/ortho.py
+++ boost/ortho.py
@@ -1,7 +1,14 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
-import feats,learn, exper.selection
+import feats,learn
+import exper.selection,exper.curve
+from exper.voting import get_datasets
 
+def acc_csv(in_path,out_path):        
+    exper.curve.acc_to_csv(in_path,out_path,get_acc)
+
+def make_plots(in_path,out_path):        
+    exper.curve.all_curves(in_path,out_path,get_acc)
 
 def get_acc(path_i):
     clf_ord=orth_selection(path_i)
@@ -13,7 +20,9 @@ def orth_selection(in_path):
     votes=feats.read_list(in_path)
     train=[ vote_i.split()[0] for vote_i in votes]
     acc_i=[person_acc(train_i) for train_i in train]
-    clf_ord=np.flip(np.argsort(acc_i))
+    print(acc_i)
+    clf_ord=np.argsort(acc_i)
+    print(clf_ord)
     return clf_ord
 
 def person_acc(train_i):
@@ -23,3 +32,8 @@ def person_acc(train_i):
     clf_i.fit(train_i.X,person_i)
     person_predict=clf_i.predict(train_i.X)
     return accuracy_score(person_i,person_predict)
+
+def exper(hc_path,deep_paths,n_feats=500):
+    deep_data=get_datasets(hc_path,deep_paths,n_feats)
+    acc=[person_acc(data_i) for data_i in deep_data]
+    print(acc)
