5d637b1daeb3c5c66d42918050238d7540fbb305
tjacek <tjacek@agh.edu.pl>
2020-03-14 21:08:20 +0100

    remove unused functions

--- exper/curve.py
+++ exper/curve.py
@@ -1,37 +1,32 @@
 import matplotlib.pyplot as plt
 import learn,feats,exper.cats,files
 
-def acc_curve(vote_path,ord,binary=False,show=True):
-    votes=selected_votes(vote_path,ord,binary)
-    n_clf=len(votes)
-    results=[exper.cats.voting(votes[:(i+1)],None) 
-                for i in range(n_clf)]
-    acc=[ learn.compute_score(result_i[0],result_i[1],False)[0] 
-            for result_i in results]
-    if(show):
-        title="_".join(vote_path.split('/'))
-        title=title.replace(".._","").replace("votes","")
-        voting_type= "HARD" if(binary) else "SOFT"
-        title+=voting_type
-        show_curve(acc,len(results),title,"test")
-    return acc
-
-def selected_votes(vote_path,ord,binary=False):
-    votes=feats.read_list(vote_path)
-    if(binary):
-        votes=[binarize(vote_i) for vote_i in votes]
-    return [ votes[k] for k in ord]
+#def acc_curve(vote_path,ord,binary=False,show=True):
+#    votes=selected_votes(vote_path,ord,binary)
+#    n_clf=len(votes)
+#    results=[exper.cats.voting(votes[:(i+1)],None) 
+#                for i in range(n_clf)]
+#    acc=[ learn.compute_score(result_i[0],result_i[1],False)[0] 
+#            for result_i in results]
+#    if(show):
+#        title="_".join(vote_path.split('/'))
+#        title=title.replace(".._","").replace("votes","")
+#        voting_type= "HARD" if(binary) else "SOFT"
+#        title+=voting_type
+#        show_curve(acc,len(results),title,"test")
+#    return acc
 
 def acc_to_csv(in_path,out_path,fun):
-    names,acc=[],[]
+    names,acc=["Accuracy for"],[]
     for path_i in files.top_files(in_path):
         name_i=path_i.split('/')[-1]
         names.append(name_i)
         acc.append(fun(path_i))
     acc=list(zip(*acc))
     csv=",".join(names)+'\n'
-    for acc_i in acc:
-        csv+= ",".join(map(str,acc_i))+'\n'
+    for i,acc_i in enumerate(acc):
+        acc_i=[round(acc_ij,4) for acc_ij in acc_i]
+        csv+= "%d,%s\n" % (i+1,",".join(map(str,acc_i)))
     file_str = open(out_path,'w')
     file_str.write(csv)
     file_str.close()
