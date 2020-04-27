f7bc19ed6038de8fd4e3fcde9ddfae3bc4635ba0
tjacek <tjacek@student.agh.edu.pl>
2020-04-27 13:58:02 +0200

    fix script

--- script.py
+++ script.py
@@ -2,20 +2,22 @@ import ens,files,exper.cats
 
 clf_type="LR"
 feats=["stats","basic","sim"]
-seq_type="ens2"
-train=False
+seq_type="ens4"
+train=True
+in_dir="../time"
+out_dir="time"
 
-files.make_dir(seq_type)
-files.make_dir("%s/%s" % (seq_type,clf_type))
+files.make_dir( "%s/%s"  %(out_dir,seq_type))
+files.make_dir("%s/%s/%s" % (out_dir,seq_type,clf_type))
 for feat_i in feats:
     for feat_j in feats:
         feat_path="../%s/%s/feats" % (seq_type,feat_j)
-        hc_path="../proj/%s/feats" % feat_i
-        vote_path="%s/%s/%s_%s"% (seq_type,clf_type,feat_i,feat_j)        
+        hc_path="%s/%s/feats" % (in_dir,feat_i)
+        vote_path="%s/%s/%s/%s_%s"% (out_dir,seq_type,clf_type,feat_i,feat_j)        
         args={'hc_path':hc_path,'deep_paths':feat_path,'n_feats':0}        
         print(vote_path)
         if(train):
-            ens.exp(args,vote_path,clf=clf_type,train=train,show=True)
+            ens.exp(args,vote_path,clf=clf_type,train=train)
         else:
-            result=exper.cats.adaptive_votes(vote_path,binary=False,show=False)
+            result=exper.cats.adaptive_votes(vote_path,binary=False,clf_type=False,show=False)
             print(result)
