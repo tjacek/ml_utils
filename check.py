2e4998be5d6fec924f9d9d08b458f2d48df307d2
tjacek <tjacek@student.agh.edu.pl>
2020-04-29 21:39:39 +0200

    ensemble stats

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
-import numpy as np
+import numpy as np,os
 import files,dataset
-#import exper.inspect,
+import exper.inspect
 
 def sparse_feats(in_path):
     seqs=[ dataset.read_dataset(path_i) 
@@ -23,4 +23,24 @@ def norm(arr):
     arr-=np.mean(arr)
     return arr/np.std(arr)
 
-sparse_feats("../old/single_smooth/binary_seq")
+def ens_inspect(vote_path):
+    paths=files.top_files(vote_path)
+    for path_i in paths:
+        print(path_i)
+        print( files.has_dirs(path_i))
+        print(ens_stats(path_i))
+
+def ens_stats(path_i):
+    acc=exper.inspect.clf_acc(path_i,data="test")
+    mean_i=np.mean(acc)
+    med_i=np.median(acc)
+    max_i,k=np.amax(acc),np.argmax(acc)
+    min_i,t=np.amin(acc),np.argmin(acc)
+    return "%s,%s,%s,%s,%s,%s"% (max_i,k,mean_i,med_i,min_i,t)
+
+#sparse_feats("../old/single_smooth/binary_seq")
+vote_path="proj2/ens3/LR"#/#stats_sim"
+
+ens_inspect(vote_path)
+#acc=exper.inspect.clf_acc(votes_path,data="test")
+#print(acc)
