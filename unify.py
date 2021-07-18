f284d075df659cb389ab4f38e20687f155ca75dd
tjacek <tjacek@student.agh.edu.pl>
2021-07-18 16:50:45 +0200

    print option

--- unify.py
+++ unify.py
@@ -1,8 +1,10 @@
+import numpy as np
 import feats,files 
 
 def unify_dataset(path,out_path):
     dataset= feats.read_unified(files.top_files(path))
     print(dataset.dim())
+    np.set_printoptions(threshold=3000)
     dataset.save(out_path)
 
 in_path="../conv_frames/test/simple_feats"
