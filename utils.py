d4cbf810cac14a8606596cf9d5156c6b4522593d
tjacek <tjacek@student.agh.edu.pl>
2021-07-25 15:28:34 +0200

    ens acc

--- utils.py
+++ utils.py
@@ -1,19 +1,23 @@
 import numpy as np
-import feats
+import feats,learn,files
 
 def sum_data(paths):
     part_feats=[feats.read(path_i)[0] for path_i in paths]
     full_feats=feats.Feats()
     for name_i in part_feats[0].keys():
-        id_i="%s_1"%name_i  #name_i. subname(3)
-        raw_i=[ path_i[id_i] for path_i in part_feats]
-        data_i=np.sum(raw_i,axis_i=0)
-        raise Exception(data_i.shape)
+        raw_i=[ path_i[name_i] for path_i in part_feats]
+        data_i=np.sum(raw_i,axis=0)
         full_feats[name_i]=data_i
     return full_feats
 
-true_path="../3DHOI/1D_CNN/feats"
-pred_path="student/ae_30_200/feats"
-out_path="student/ae_30_200/res"
-paths=[,"../conv_frames/student/ae_30_200/feats","../conv_frames/student/ae_30_200/next/feats"]
-sum_data(paths)
+def ens_acc(in_path):
+    import os.path
+    for path_i in files.top_files(in_path):
+        in_i="%s/feats" % path_i
+        if(os.path.isfile(in_i)): 
+            result_i=learn.train_model(in_i)#feats.read(in_i)[0]
+            print(result_i.get_acc())
+
+
+in_path="../conv_frames/early/"
+ens_acc(in_path)
