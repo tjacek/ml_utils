cb6ef2810ae3659dccb8451d28d2c88ead80b6b2
tjacek <tjacek@agh.edu.pl>
2021-10-29 23:31:05 +0200

    shapelets helper

--- shapelets.py
+++ shapelets.py
@@ -1,11 +1,44 @@
 from tslearn.shapelets import LearningShapelets
+import numpy as np
 import files,seqs,feats
+import timeit
+
+def make_feats(in_path):
+    model=train_model(in_path)
+    def helper(name_i,data_i):
+        print(name_i)
+        frames=seqs.subsample(data_i,36)
+        frames= np.squeeze(frames)
+        frames=np.expand_dims(frames,axis=0)
+        distances = model.transform(frames)
+        return distances
+    seqs.transform_lazy(in_path,helper,out_path="feats.txt")
+
+def train_model(in_path):
+    paths=select_paths(in_path)
+    paths=paths[:3] + paths[-3:]
+    ts=seqs.from_paths(paths)
+    ts.subsample(36)
+    model = LearningShapelets(n_shapelets_per_size=None,max_iter=2) #{3: 40})
+    X,y,names=ts.as_dataset()
+    model.fit(X,y)
+    return model
+
+def select_paths(in_path):
+    paths=[]
+    persons=set(["4","5","6"])
+    for path_i in files.top_files(in_path):
+        name_i=files.get_name(path_i)
+        print(name_i)
+        if(name_i.get_person()==1):
+            person_i=name_i.split("_")[2]
+            if(person_i in persons):
+                paths.append(path_i)
+    return paths
 
 def compute_shaplets(in_path,out_path,n_feats=40):
     ts=seqs.read_seqs(in_path)
-    ts.subsample(36)
-#    raise Exception(ts.shape())
-#    ts.resize(64)
+    ts.resize(64)
     train,test=ts.split()
     model = LearningShapelets(n_shapelets_per_size={3: n_feats})
     train_X,train_y,train_names=train.as_dataset()
@@ -26,7 +59,10 @@ def feat_exp(in_path,out_path,n=20,step=10):
         out_i="%s/%d" % (out_path,n_feats)	
         compute_shaplets(in_path,out_i,n_feats= n_feats)	
 
-
-in_path="../conv_frames/seqs"
+in_path="shape_32"#"../conv_frames/seqs"
 out_path="../conv_frames/feats"
-feat_exp(in_path,out_path)
+#feat_exp(in_path,out_path)
+start = timeit.timeit()
+make_feats(in_path)
+end = timeit.timeit()
+print(end - start)
