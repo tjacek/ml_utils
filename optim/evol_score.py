e5d43cf34c0334c72be9a619c4fb533b1cfb5d34
tjacek <tjacek@agh.edu.pl>
2021-12-15 23:24:44 +0100

    eval class

--- optim/evol_score.py
+++ optim/evol_score.py
@@ -4,11 +4,23 @@ import numpy as np
 from scipy.optimize import differential_evolution
 import selection,pref,ens
 
+class GenAlg(object):
+    def __init__(self,maxiter=10):
+        self.maxiter=maxiter
+
+    def __call__(self,loss_fun,n_cand,maxiter=50):
+        bound_w = [(0.0, n_cand)  for _ in range(n_cand)]
+        result = differential_evolution(loss_fun, bound_w, 
+#        init=init_score(15,n_cand),
+                 maxiter=self.maxiter, tol=1e-7)
+        return result['x']
+
 def exp(paths,n_iters):
+    optim=GenAlg()
     old_results,new_results=[],[]
     for i in range(n_iters):
         old_i,s_clf=selection.select_clfs(paths,read_type=None)
-        new_i,score_i=find_score(paths,s_clf)
+        new_i,score_i=find_score(paths,s_clf,optim)
         old_results.append(old_i)
         new_results.append(new_i)
     acc=[result_i.get_acc() for result_i in new_results]
@@ -16,7 +28,7 @@ def exp(paths,n_iters):
     new_results[k].report()
     print(old_results[k].get_acc())
 
-def find_score(paths,ensemble):
+def find_score(paths,ensemble,optim):
     ensemble=ens.get_ensemble_helper(ensemble)
     result,votes=ensemble(paths)
     pref_dict=pref.to_pref(votes.results)
@@ -26,17 +38,10 @@ def find_score(paths,ensemble):
         acc=result.get_acc()
         print(acc)
         return (1.0-acc)
-    score=optim(loss_fun,n_cand,maxiter=10)
+    score=optim(loss_fun,n_cand)#,maxiter=10)
     print(score)
     result=eval_score(score,n_cand,pref_dict)
     return result,score
-
-def optim(loss_fun,n_cand,maxiter=50):
-    bound_w = [(0.0, n_cand)  for _ in range(n_cand)]
-    result = differential_evolution(loss_fun, bound_w, 
-#        init=init_score(15,n_cand),
-        maxiter=maxiter, tol=1e-7)
-    return result['x']
 
 def eval_score(score,n_cand,pref_dict):
     def system_i(name_i,pref_dict):
