4f1e745c654e9ac28d6307e1f3936aba42003ae7
tjacek <tjacek@agh.edu.pl>
2021-12-16 15:09:07 +0100

    init_population

--- optim/evol_score.py
+++ optim/evol_score.py
@@ -5,23 +5,36 @@ from scipy.optimize import differential_evolution
 import selection,pref,ens
 
 class GenAlg(object):
-    def __init__(self,maxiter=10,init='latinhypercube'):
+    def __init__(self,maxiter=10,init_type='latinhypercube',pop_size=15):
         self.maxiter=maxiter
-        self.init=init
+        self.init_type=init_type
+        self.pop_size=pop_size
  
-    def __call__(self,loss_fun,n_cand,maxiter=50):
-        if(self.init=="borda"):
-            self.init=[[n_cand-j
-                          for j in range(n_cand)]
-                              for _ in range(15)]	
+    def __call__(self,loss_fun,n_cand):
+        init=init_population(self.init_type,n_cand,self.pop_size)
         bound_w = [(0.0, n_cand)  for _ in range(n_cand)]
         result = differential_evolution(loss_fun, bound_w, 
-            init=self.init,
+            init=init,
             maxiter=self.maxiter, tol=1e-7)
         return result['x']
 
-def exp(paths,n_iters):
-    optim=GenAlg(init="borda")
+def init_population(init_type,n_cand,pop_size=15):
+    if(init_type=="borda"):
+        return [[n_cand-j
+                    for j in range(n_cand)]
+                        for _ in range(pop_size)]
+    if(init_type=="borda_mixed"):
+        pop=[]
+        for i in range(pop_size):
+            if( (i%2)==0):
+                pop.append([n_cand-j for j in range(n_cand)])
+            else:
+                pop.append( np.random.uniform(low=0.0, 
+                    high=n_cand, size=(pop_size,)))
+        return pop
+    return 'latinhypercube'
+
+def exp(paths,n_iters,optim):
     old_results,new_results=[],[]
     for i in range(n_iters):
         old_i,s_clf=selection.select_clfs(paths,read_type=None)
@@ -60,4 +73,5 @@ if __name__ == "__main__":
                 path % "shapelets"]
     binary=path % "ens/splitI/"
     paths=(common,binary)
-    exp(paths,10)
+    optim=GenAlg(init_type="borda")
+    exp(paths,10,optim)
