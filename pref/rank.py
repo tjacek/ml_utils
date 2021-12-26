a8be28684558804d17faaf4cd9ea1212a30e3b82
tjacek <tjacek@agh.edu.pl>
2021-12-26 21:48:55 +0100

    rank

--- pref/rank.py
+++ pref/rank.py
@@ -11,22 +11,26 @@ def get_ranks(paths):
     ensemble=ens.get_ensemble_helper(ensemble=None)
     result,votes=ensemble(paths)
     votes_dict= votes.as_dict()
+    n_cats=result.n_cats()
+#    check(votes_dict,n_cats)
     def helper(cat_i,clf_j):
         names=list(votes_dict.keys())
-        values=[ votes_k[clf_j][cat_i] 
-                   for name_k,votes_k in votes_dict.items()]
-        return [(names[t],t) for t in np.flip(np.argsort(values))]
-    print(helper(0,0))
-#def get_ranks(paths):
-#    common=paths[0]
-#    dataset= ens.read_dataset(common,None)[0]
-#    result=learn.train_model(dataset,clf_type="LR")
-#    rank_dict=to_rank(result)
-#    pairs_transform(rank_dict,dataset)
+        values=[ votes_dict[name_k][clf_j][cat_i] 
+                   for name_k in names]
+        return dict([(names[t],t) 
+            for t in np.flip(np.argsort(values))])
+    best_dicts=[helper(0,clf_i) for clf_i in range(n_cats)]
+    pref_dict={ name_i:[dict_j[name_i] 
+                        for dict_j in best_dicts]  
+                            for name_i in best_dicts[0]}
+    print(pref_dict)
 
-#def to_rank(result):
-#	return { name_i:np.flip(np.argsort(y_i)) 
-#		for name_i,y_i in zip(result.names,result.y_pred)}
+def check(votes_dict,n_cats):
+    test_name=list(votes_dict.keys())[0]
+    comp_rank=[votes_dict[test_name][clf_j][0]
+                for clf_j in range(n_cats)]
+    raise Exception(comp_rank)
+
 
 #def pairs_transform(rank_dict,dataset):
 #    X,y=[],[]
@@ -44,6 +48,7 @@ if __name__ == "__main__":
     path="../../VCIP/3DHOI/%s/feats"
     common=[path % "1D_CNN",#"../deep_dtw/dtw"]
          path % "shapelets"]
+    common=None
     binary=path % "ens/splitII/"
     paths=(common,binary)
     get_ranks(paths)
