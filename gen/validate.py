6b81c614e11dbef560803cfa0c237d07783e66e9
tjacek <tjacek@agh.edu.pl>
2022-01-12 23:16:33 +0100

    validate decorator

--- gen/validate.py
+++ gen/validate.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append("..")
-import learn,ens,files,dataset,convert,bag
+import learn,dataset,convert,bag
+import files,ens
 
-def make_validate(dataset_gen,out_path,clf="LR"):
-    results=learn.person_votes(dataset_gen,
-    	get_partial=enum_partial,clf=clf,n_split=5)
-    validate_votes=ens.Votes(results)
-    files.save(out_path,validate_votes)
+#def make_validate(dataset_gen,out_path,clf="LR"):
+#    results=learn.person_votes(dataset_gen,
+#    	get_partial=enum_partial,clf=clf,n_split=5)
+#    validate_votes=ens.Votes(results)
+#    files.save(out_path,validate_votes)
 
 def enum_partial(train,n_split,clf):
     for p in range(n_split):
@@ -15,18 +16,31 @@ def enum_partial(train,n_split,clf):
         validate,validate_test=train.split(selector_p)
         yield learn.make_result(validate,validate_test,clf)
 
-def bag_validate(dataset,out_path,clf="LR"):
-    train,test=dataset.split()
-    results=[]
-    for data_i in bag.resample_dataset(dataset):
-        model_i=learn.train_model(data_i,clf_type=clf,model_only=True)
-        y_pred=model_i.predict_proba(test.get_X())
-        result_i= learn.Result(train.get_labels(),y_pred,train.names())    
-        results.append(result_i)
+@files.dir_function
+def make_validate(in_path,out_path):
+    print(in_path)
+    print(out_path)
+    dataset_gen=dataset.read_dataset(in_path)
+    results=learn.person_votes(dataset_gen,
+        get_partial=enum_partial,clf="LR",n_split=5)
     validate_votes=ens.Votes(results)
-    files.save(out_path,validate_votes)
+    validate_votes.save(out_path)
+
+#def bag_validate(dataset,out_path,clf="LR"):
+#    train,test=dataset.split()
+#    results=[]
+#    for data_i in bag.resample_dataset(dataset):
+#        model_i=learn.train_model(data_i,clf_type=clf,model_only=True)
+#        y_pred=model_i.predict_proba(test.get_X())
+#        result_i= learn.Result(train.get_labels(),y_pred,train.names())    
+#        results.append(result_i)
+#    validate_votes=ens.Votes(results)
+#    files.save(out_path,validate_votes)
 
 if __name__ == "__main__":    
-    paths=([None,'penglung/common'],'penglung/binary')
-    dataset_gen=dataset.read_multi(paths)
-    make_validate(dataset_gen,'penglung/validate')
+#    paths=([None,'penglung/common'],'penglung/binary')
+    in_path=("../../data/common","../../data/binary")
+    out_path="../../data/valid"
+    make_validate(in_path,out_path)
+#    dataset_gen=dataset.read_multi(paths)"../../data/common"
+#    make_validate(dataset_gen,'penglung/validate')
