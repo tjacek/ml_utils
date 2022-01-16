a3a9cd78af315b2304e73e692bc45ba275d1ada9
tjacek <tjacek@agh.edu.pl>
2022-01-16 18:41:10 +0100

    use decorator in check

--- check.py
+++ check.py
@@ -1,24 +1,13 @@
 import numpy as np,os
-import feats
+import feats,files
 
+@files.dir_function
 def check_feats(in_path):
-    if(type(in_path)==tuple):
-        common,binary=in_path
-        check_ens(common,binary)
-        return
+    name_i=in_path.split('/')[-1]
+    name_i=name_i.split(".")[0]
     feat_dict=feats.read(in_path)[0]
-    print("size:%d,dim:%d" % (len(feat_dict),feat_dict.dim()[0]))
+    n_cats=feat_dict.names().n_cats()
+    print(f"{name_i},{n_cats},{feat_dict}")
 
-def check_ens(common,binary):
-    common=feats.read(common)[0].dim()[0]
-    binary=feats.read(binary)[0].dim()[0]
-    print("common:%d,binary:%d" % (common,binary))
-    print("sum:%d" % (common+binary))
-
-dir_path="../3DHOI/"
-binary_path="%s/ens/I/feats" % dir_path
-base_path="%s/1D_CNN/feats" % dir_path
-dtw_path="../deep_dtw/dtw"
-ae_path="../best2/3_layers/feats"
-
-check_feats(([base_path,dtw_path],binary_path))
+in_path="../data/I/common"
+check_feats(in_path)
