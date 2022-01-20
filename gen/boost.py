043d97c53598538ce82ca812062b2c3b6ca3a903
tjacek <tjacek@agh.edu.pl>
2022-01-20 16:50:03 +0100

    test model

--- gen/boost.py
+++ gen/boost.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append("..")
 import xgboost as xgb
-import files,feats
-from sklearn.metrics import accuracy_score
+import files,feats,learn,exp
 
+@exp.save_results
 @files.dir_function(args=1,recreate=False)
 def make_datasets(in_path):
     print(in_path)
@@ -18,10 +18,34 @@ def make_datasets(in_path):
     'objective':'multi:softmax','num_class':data_i.names().n_cats()}
     evallist = [(dtest, 'eval'), (dtrain, 'train')]
     bst = xgb.train(param, dtrain, num_round, evallist)
-    ypred = bst.predict(dtest)
-    acc_i=accuracy_score(y,ypred) 
-    return acc_i
+    y_pred = bst.predict(dtest)
+    result_i=learn.Result(y,y_pred,names)
+    return (in_path.split('/')[-1],result_i)
+
+
+@files.dir_function(args=1,recreate=False)
+def make_models(in_path):#,out_path):
+    print(in_path)
+    data_i=feats.read(in_path)[0]
+    train_i,test_i=data_i.split()
+    X,y,names=train_i.as_dataset() 
+    n_estimators = 10
+    max_depth = 10
+    model = xgb.XGBClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        min_child_weight=1)
+    model.fit(X, y)
+    booster = model.get_booster()
+    return test_model(model,test_i)
+
+def test_model(model,test):
+    from sklearn.metrics import accuracy_score
+    X,y,names=test.as_dataset() 
+#   dtest = xgb.DMatrix(X, label=y)
+    y_pred = model.predict(X)
+    return accuracy_score(y,y_pred)
 
 if __name__ == "__main__":
-    acc=make_datasets("../../data/II/common")#,"../../data/II/boost")
+    acc=make_models("../../data/II/common")#,"../../data/II/boost")
     print(acc)
