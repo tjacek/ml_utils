52a52fd81e96b262a16ee0fe6709a27841abb200
tjacek <tjacek@agh.edu.pl>
2022-01-21 15:10:27 +0100

    find params

--- gen/boost.py
+++ gen/boost.py
@@ -34,7 +34,7 @@ def make_models(in_path,out_path):
     train_i,test_i=data_i.split()
     train_tuple=train_i.as_dataset() 
     n_cats=data_i.n_cats()
-    clf_i=train_boost(train_tuple[0],train_tuple[1])
+    clf_i,best_params=train_boost(train_tuple[0],train_tuple[1])
     test_tuple=test_i.as_dataset()
     
     out_valid,out_test=out_path
@@ -46,7 +46,7 @@ def make_models(in_path,out_path):
 
     result=votes_i.voting()
     result.report()
-    return result
+    return (in_path.split("/")[-1],result.get_acc(),best_params)
 
 def get_boost_votes(result_tuple,clf_i):
     X,y_true,names=result_tuple
@@ -64,13 +64,14 @@ def get_boost_votes(result_tuple,clf_i):
     return ens.Votes(results)
 
 def train_boost(X_train,y_train):
-    clf_i = ensemble.GradientBoostingClassifier(max_depth=3)
-#    clf_i = GridSearchCV(alg,{'max_depth': [2,4,6],
-#                    'n_estimators': [5,10,20]},
-#                    verbose=1,
-#                    scoring='neg_log_loss')
+    clf_i = ensemble.GradientBoostingClassifier()#max_depth=3)
+    clf_i = GridSearchCV(clf_i,{'max_depth': [2,4,6],
+                    'n_estimators': [5,10,15,20]},
+                    verbose=1,
+                    scoring='neg_log_loss')
     clf_i.fit(X_train,y_train)    
-    return clf_i#.best_estimator_
+    best_params=clf_i.cv_results_['params'][0]
+    return clf_i.best_estimator_,best_params
 
 def test_model(model,test):
     from sklearn.metrics import accuracy_score
@@ -84,4 +85,4 @@ if __name__ == "__main__":
     results=make_models("../../data/II/common",("valid","test"))#,"models")
 #    prepare_votes("models")
     for result_i in results:
-        print(result_i.get_acc())
+        print(result_i)
