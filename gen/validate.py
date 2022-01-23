28abcde3cd4e3ce3e9988e379666c0014b03297b
tjacek <tjacek@agh.edu.pl>
2022-01-23 23:30:23 +0100

    check votes

--- gen/validate.py
+++ gen/validate.py
@@ -28,12 +28,19 @@ def make_test(in_path,out_path):
     final_votes=dataset.eff_voting(in_path,clf="LR")
     final_votes.save(out_path)
     return len(final_votes)
+    
+@files.dir_function(args=1)
+def check_votes(votes_path):
+    import pickle
+    with open(votes_path, 'rb') as votes_file:
+        votes=pickle.load(votes_file)
+        acc= [result_i.get_acc() for result_i in votes.results]
+        print(acc)
 
 if __name__ == "__main__":    
 #    paths=([None,'penglung/common'],'penglung/binary')
     in_path=("../../data/II/common2","../../data/II/binary")
     out_path="binary/valid"
-    valid_size=make_validate(in_path,out_path)
-    test_size= make_test(in_path,"binary/test")
-    print(valid_size)
-    print()
+#    valid_size=make_validate(in_path,out_path)
+#    test_size= make_test(in_path,"binary/test")
+    check_votes(out_path)
