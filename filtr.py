6e8d739d286ba409d73f94864c2abed64b1bb2c9
tjacek <tjacek@agh.edu.pl>
2022-08-08 03:46:31 +0200

    filter

--- filtr.py
+++ filtr.py
@@ -3,6 +3,29 @@ from sklearn.neighbors import KNeighborsClassifier
 from collections import defaultdict
 import feats,dtw,files
 
+class AllResults(object):
+    def __init__(self):
+        self.keys=[]
+        self.results=[]
+    
+    def filtr(self,value=None,k=0):
+        if(value is None):
+            return range(len(self.keys))
+        return [i for i,key_i in enumerate(self.keys)
+                    if(key_i[k]==value)]
+
+    def add(self,key_i,result_i):
+        self.keys.append(key_i)
+        self.results.append(result_i)
+
+def make_all_results(raw):
+    all_results=AllResults()
+    for path_i,output in raw:
+#        print(output)
+        for  key_j,result_j in zip(*output):
+            all_results.add(key_j,result_j)
+    return all_results
+
 class Rename(object):
     def __init__(self,elements,index=2):
         self.elements=set(elements)
@@ -10,7 +33,7 @@ class Rename(object):
 
     def __call__(self,name_i):
         digits=name_i.split('_')#digits()
-        train= not (digits[self.index] in self.elements)
+        train= (digits[self.index] in self.elements)
         digits[1]=str(int(train))
         return files.Name("_".join(digits))
 
@@ -39,18 +62,20 @@ def exp(in_path):
     splits=[Rename(['1','2'],3),
             Rename(['1','2','5'],3),
             Rename(['1','2','3','5'],3)]
-    results_dict={}
+    keys,results=[],[]#AllResults()
     for i,split_i in enumerate(splits):
-#        raw_feats.check()
         data_i=raw_feats.rename(split_i)
-#        data_i.check()
         result_i=dtw.test_dtw(data_i)
-        results_dict[i]=result_i
-    return results_dict
+        keys.append([in_path,i])
+        results.append(result_i)
+#        all_results.add([in_path,] ,result_i)
+#        results_dict[i]=result_i
+    return (keys,results)
 
 
 if __name__ == "__main__":
     in_path="dtw"
-    results=dict(exp(in_path))
-#    print(results)
-    files.show_dict(results,fun=lambda x:x.get_acc())
+    results=make_all_results(exp(in_path))
+#    print(results.keys)
+    print(results.filtr(value=0,k=1))
+#    files.show_dict(results,fun=lambda x:x.metrics())
